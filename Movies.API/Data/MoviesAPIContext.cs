f237d11ae1f588e7037b4478577ea93e976e74a0
Pubudu Senarathne <pubudutesla@gmail.com>
2021-06-24 16:41:01 +0530

    Added controller and seeded inmemory database

---
+++ Movies.API/Data/MoviesAPIContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Movies.API.Data
+{
+    public class MoviesAPIContext : DbContext
+    {
+        public MoviesAPIContext(DbContextOptions<MoviesAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.API.Models.Movie> Movie { get; set; }
+    }
+}
--- Movies.API/Startup.cs
+++ Movies.API/Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Movies.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Movies.API
 {
@@ -32,6 +34,9 @@ namespace Movies.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies.API", Version = "v1" });
             });
+
+            services.AddDbContext<MoviesAPIContext>(options =>
+            options.UseInMemoryDatabase("Movies"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
