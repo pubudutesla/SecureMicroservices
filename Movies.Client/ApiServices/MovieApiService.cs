2f2047c7374ae39e70e1d45675f1494fe6b106a0
Pubudu Senarathne <pubudutesla@gmail.com>
2021-06-24 19:24:51 +0530

    Added and implemented IMovieApiServiceand removed EFCore dependencies since the client will consume Movies API in future

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(
+                new Movie
+                {
+                    Title= "Avengers: End Game",
+                    Genre= "Sci-Fi",
+                    Rating= "10.0",
+                    ReleaseDate= DateTime.Now,
+                    ImageUrl= "images/src",
+                    Owner= "tesla"
+                });
+
+            return await Task.FromResult(movieList);
+        }
+
+        Task<Movie> IMovieApiService.CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IMovieApiService.DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Movie> IMovieApiService.GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Movie> IMovieApiService.UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Movies.Client.Data;
+using Movies.Client.ApiServices;
 
 namespace Movies.Client
 {
@@ -27,8 +21,7 @@ namespace Movies.Client
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<MoviesClientContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MoviesClientContext")));
+            services.AddScoped<IMovieApiService, MovieApiService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
